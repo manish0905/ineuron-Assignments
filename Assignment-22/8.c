c3c64b56e00873c8dd6689f34a29f101e6ca545f
manish0905 <96387598+manish0905@users.noreply.github.com>
2022-09-03 14:27:50 +0530

    Add files via upload

---
+++ Assignment-22/8.c
@@ -0,0 +1,36 @@
+//Write a program to demonstrate dangling pointers in C
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int n, *p,i=0;
+
+    printf("Enter the no. of values to be entered:");
+    scanf("%d", &n);
+    p=(int*)calloc(n,sizeof(int));
+
+    if(p==NULL)
+    {
+        printf("ERROR, Memory allocation failed. \n");
+        return 0;
+    }
+
+    printf("Enter the %d Values: ",n);
+    while(i<n)
+    {
+        scanf("%d",p+i);
+
+        i++;
+    }
+
+    printf("The entered values are: ");
+    for(i=0;i<n;i++)
+        printf("%d ",*(p+i));
+
+    printf("Value of P before: %d \n",p);
+    free(p);
+    printf("Value of P after: %d \n",p);
+
+    return 0;
+}
